57a2e45af99154a34b659ab6214bf71974ad1a8c
Abdelkarim Aoualallay <aoulallayabdelkarim@gmail.com>
2026-03-29 23:49:26 +0100

    Add: save new todo
    Add saving new todos with action creator middleware function

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -1,18 +1,38 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { saveNewTodo } from '../todos/todosSlice'
 
 const Header = () => {
+  const dispatch = useDispatch()
   const [text, setText] = useState('')
 
   const handleChange = (e) => setText(e.target.value)
 
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    // If the user pressed the Enter key:
+    const trimmedText = text.trim()
+    if (trimmedText) {
+      // Create the thunk function with the text the user wrote
+      const saveNewTodoThunk = saveNewTodo(trimmedText)
+      // Then dispatch the thunk function itself
+      dispatch(saveNewTodoThunk)
+      setText('')
+    }
+  }
+
   return (
     <header className="header">
-      <input
-        className="new-todo"
-        placeholder="What needs to be done?"
-        value={text}
-        onChange={handleChange}
-      />
+      <form onSubmit={onSubmit}>
+        <input
+          className="new-todo"
+          placeholder="What needs to be done?"
+          value={text}
+          onChange={handleChange}
+        />
+        <button></button>
+      </form>
     </header>
   )
 }
--- src/features/todos/todosSlice.js
+++ src/features/todos/todosSlice.js
@@ -2,23 +2,11 @@ import { client } from '../../api/client'
 
 const initialState = []
 
-function nextTodoId(todos) {
-  const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1)
-  return maxId + 1
-}
-
 export default function todosReducer(state = initialState, action) {
   switch (action.type) {
     case 'todos/todoAdded': {
-      // Can return just the new todos array - no extra object around it
-      return [
-        ...state,
-        {
-          id: nextTodoId(state),
-          text: action.payload,
-          completed: false,
-        },
-      ]
+      // Return a new todos state array with the new todo item at the end
+      return [...state, action.payload]
     }
     case 'todos/todoToggled': {
       return state.map((todo) => {
@@ -68,4 +56,15 @@ export default function todosReducer(state = initialState, action) {
 export async function fetchTodos(dispatch, getState) {
   const response = await client.get('/fakeApi/todos')
   dispatch({ type: 'todos/todosLoaded', payload: response.todos })
+}
+
+// Write a synchronous outer function that receives the `text` parameter:
+export function saveNewTodo(text) {
+  // And then creates and returns the async thunk function:
+  return async function saveNewTodoThunk(dispatch, getState) {
+    // ✅ Now we can use the text value and send it to the server
+    const initialTodo = { text }
+    const response = await client.post('/fakeApi/todos', { todo: initialTodo })
+    dispatch({ type: 'todos/todoAdded', payload: response.todo })
+  }
 }
