c1ec52e11c988640c79c7ea8f8681ff5718cdbb0
Abdelkarim Aoualallay <aoulallayabdelkarim@gmail.com>
2026-03-29 19:33:30 +0100

    Init commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client' // 1. Updated import
+import './index.css'
+import App from './App'
+
+import './api/server'
+
+import store from './store'
+import { Provider } from 'react-redux'
+
+// 2. Select the DOM element
+const container = document.getElementById('root')
+
+// 3. Create the React 18 root and render
+const root = createRoot(container)
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+)
