8bb158ea0bf6701c00d3d61a0473f0bcedef2811
Abdelkarim Aoualallay <aoulallayabdelkarim@gmail.com>
2026-03-29 20:50:01 +0100

    pref: optimize TodoList rendering using ID-based selection
    Refactored TodoList and TodoListItem to improve rendering performance.
    
    - Changed TodoList to select an array of todo IDs instead of full objects.
    - Implemented shallowEqual in useSelector to prevent TodoList re-renders when todo content changes.
    - Updated TodoListItem to receive an 'id' prop and select its own data from the store.

--- src/features/todos/TodoList.js
+++ src/features/todos/TodoList.js
@@ -1,14 +1,14 @@
-import { useSelector } from 'react-redux'
+import React from 'react'
+import { useSelector, shallowEqual } from 'react-redux'
 import TodoListItem from './TodoListItem'
 
-const selectTodos = (state) => state.todos
+const selectTodoIds = (state) => state.todos.map((todo) => todo.id)
 
 const TodoList = () => {
-  const todos = useSelector(selectTodos)
+  const todoIds = useSelector(selectTodoIds, shallowEqual)
 
-  // since `todos` is an array, we can loop over it
-  const renderedListItems = todos.map((todo) => {
-    return <TodoListItem key={todo.id} todo={todo} />
+  const renderedListItems = todoIds.map((todoId) => {
+    return <TodoListItem key={todoId} id={todoId} />
   })
 
   return <ul className="todo-list">{renderedListItems}</ul>
